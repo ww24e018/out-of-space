151ed8a5f38cd94c41e35a3dbe3247555e3c5a4f
Florian Scholz <idlinglight@pm.me>
2026-03-13 22:21:09 +0100

    converge on 2 hslrad variants.

--- src/renderer/src/visualisation/palettes.ts
+++ src/renderer/src/visualisation/palettes.ts
@@ -34,7 +34,39 @@ export const solarizedPalette: ColorPalette = {
   directories: ['#073642', '#0a4050', '#0d4d5e']
 }
 
+export const hslRadialVariant2: ColorPalette = {
+  name: 'HSL Radial 0°/37%/19% CA',
+  categories: {
+    code: 'hsl(135, 37%, 19%)',
+    image: 'hsl(180, 37%, 19%)',
+    document: 'hsl(225, 37%, 19%)',
+    archive: 'hsl(0, 37%, 19%)',
+    media: 'hsl(270, 37%, 19%)',
+    config: 'hsl(45, 37%, 19%)',
+    data: 'hsl(315, 37%, 19%)',
+    other: 'hsl(90, 37%, 19%)'
+  },
+  directories: ['hsl(315, 0%, 19%)', 'hsl(315, 0%, 16%)', 'hsl(315, 0%, 13%)']
+}
+
+export const hslRadialVariant3: ColorPalette = {
+  name: 'HSL Radial 9 step 298°/65%/21% IS',
+  categories: {
+    code: 'hsl(298, 65%, 21%)',
+    image: 'hsl(338, 65%, 21%)',
+    document: 'hsl(18, 65%, 21%)',
+    archive: 'hsl(58, 65%, 21%)',
+    media: 'hsl(98, 65%, 21%)',
+    config: 'hsl(138, 65%, 21%)',
+    data: 'hsl(178, 65%, 21%)',
+    other: 'hsl(218, 65%, 21%)'
+  },
+  directories: ['hsl(258, 65%, 21%)', 'hsl(258, 65%, 19%)', 'hsl(258, 65%, 17%)']
+}
+
 export const palettes: Record<string, ColorPalette> = {
   solarized: solarizedPalette,
-  legacy: legacyPalette
+  legacy: legacyPalette,
+  hslRad8_1: hslRadialVariant2,
+  hslRad9_1: hslRadialVariant3
 }
